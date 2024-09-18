6f53f564adf1b58be6cc8171c03daf1674bc031d
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2024-09-18 12:29:41 +0530

    render props pattern for tooltip

--- app/components/Tooltip.jsx
+++ app/components/Tooltip.jsx
@@ -1,28 +1,46 @@
 import * as React from "react";
 import PropTypes from "prop-types";
 import withHover from "./withHover";
+import Hover from "./Hover";
 
 const container = {
   position: "relative",
   display: "flex",
 };
 
-class Tooltip extends React.Component {
-  render() {
-    const { children, element, hovering } = this.props;
-
-    return (
-      <div style={container}>
-        {hovering === true && element}
-        {children}
-      </div>
-    );
-  }
+// With Render Props
+export default function Tooltip({ children, element }) {
+  return (
+    <Hover>
+      {(hovering) => {
+        return (
+          <div style={container}>
+            {hovering === true && element}
+            {children}
+          </div>
+        );
+      }}
+    </Hover>
+  );
 }
+
+// With HOC
+// class Tooltip extends React.Component {
+//   render() {
+//     const { children, element, hovering } = this.props;
+
+//     return (
+//       <div style={container}>
+//         {hovering === true && element}
+//         {children}
+//       </div>
+//     );
+//   }
+// }
+
+// export default withHover(Tooltip);
 
 Tooltip.propTypes = {
   children: PropTypes.node.isRequired,
   element: PropTypes.node.isRequired,
 };
-
-export default withHover(Tooltip);
