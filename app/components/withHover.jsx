514973b1fe2eb2d34dcd64148122fbe3c4b5b3b8
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2024-09-18 11:36:19 +0530

    hoc added for tooltip

---
+++ app/components/withHover.jsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+
+export default function withHover(Component) {
+  return class WithHover extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        hovering: false,
+      };
+
+      this.mouseOver = this.mouseOver.bind(this);
+      this.mouseOut = this.mouseOut.bind(this);
+    }
+    mouseOver() {
+      this.setState({ hovering: true });
+    }
+    mouseOut() {
+      this.setState({ hovering: false });
+    }
+    render() {
+      return (
+        <div onMouseOver={this.mouseOver} onMouseOut={this.mouseOut}>
+          <Component {...this.props} hovering={this.state.hovering} />
+        </div>
+      );
+    }
+  };
+}
